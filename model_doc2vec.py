9c19a4e6650c882f28a0ad83d3869908528ded34
raihan2108 <raihan2108@gmail.com>
2015-11-26 14:02:25 -0500

    committed

---
+++ model_doc2vec.py
@@ -0,0 +1,114 @@
+# gensim modules
+from gensim import utils
+from gensim.models.doc2vec import LabeledSentence
+from gensim.models import Doc2Vec
+
+# numpy
+import numpy as np
+import json
+from os import path
+import util
+# classifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.cross_validation import KFold
+
+n_fold = 5
+
+class LabeledLineSentence(object):
+    def __init__(self, sources):
+        self.sources = sources
+
+        flipped = {}
+
+        # make sure that keys are unique
+        for key, value in sources.items():
+            if value not in flipped:
+                flipped[value] = [key]
+            else:
+                raise Exception('Non-unique prefix encountered')
+
+    def __iter__(self):
+        for source, prefix in self.sources.items():
+            with utils.smart_open(source) as fin:
+                for item_no, line in enumerate(fin):
+                    yield LabeledSentence(utils.to_unicode(line).split(), [prefix + '_%s' % item_no])
+
+    def to_array(self):
+        self.sentences = []
+        for source, prefix in self.sources.items():
+            with utils.smart_open(source) as fin:
+                for item_no, line in enumerate(fin):
+                    self.sentences.append(LabeledSentence(utils.to_unicode(line).split(), [prefix + '_%s' % item_no]))
+        return self.sentences
+
+    def sentences_perm(self):
+        return np.random.permutation(self.sentences)
+
+
+def write_data(full_file_name):
+    total_pos = 0
+    total_neg = 0
+    raw_file = open(full_file_name, 'r')
+    data = json.load(raw_file)
+    w1 = open('pos.txt', 'wb+')
+    w2 = open('neg.txt', 'wb+')
+    for entry in data:
+        rat = entry['rat']
+        if float(rat) == 1.0:
+            w1.write(entry['rev'].encode('utf-8') + '\n')
+            total_pos += 1
+        else:
+            w2.write(entry['rev'].encode('utf-8') + '\n')
+            total_neg += 1
+    return total_pos, total_neg
+
+
+def execute_model(X, y):
+    print X.shape, y.shape
+    kf = KFold(y.shape[0], n_folds=n_fold, shuffle=True)
+    results_user = np.array([0.0, 0.0, 0.0])
+    for train_index, test_index in kf:
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        precision, recall, f1 = prediction_model(X_train, y_train, X_test, y_test)
+        results_user[0] += precision
+        results_user[1] += recall
+        results_user[2] += f1
+    results_user /= n_fold
+    return results_user
+
+
+def prediction_model(X_train, y_train, X_test, y_test):
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
+    pred_labels = classifier.predict(X_test)
+    precision, recall, f1, supp = precision_recall_fscore_support(y_test, pred_labels, average='weighted')
+    return precision, recall, f1
+
+
+if __name__ == '__main__':
+    total_pos, total_neg = write_data(path.join(util.data_path, util.file_name))
+    # print total_neg, total_pos
+    sources = {'pos.txt': 'POS', 'neg.txt': 'NEG'}
+    sentences = LabeledLineSentence(sources)
+    model = Doc2Vec(size=100, alpha=0.025, min_alpha=0.025, window=5)
+    model.build_vocab(sentences.to_array())
+    for epoch in xrange(0, 10):
+        model.train(sentences)
+        print 'end of iteration ', epoch + 1
+    X = []
+    y = []
+
+    for i in xrange(0, total_pos):
+        prefix_pos = 'POS' + '_%s' % i
+        X.append(model.docvecs[prefix_pos])
+        y.append(1)
+    for i in xrange(0, total_neg):
+        prefix_neg = 'NEG' + '_%s' % i
+        X.append(model.docvecs[prefix_neg])
+        y.append(0)
+    X = np.asarray(X)
+    y = np.asarray(y)
+    results = execute_model(X, y)
+    print results
--- model_word2vec.py
+++ model_word2vec.py
@@ -1,7 +1,8 @@
 import json
+from gensim import utils
 from os import path
 import util
-from gensim.models import doc2vec
+from gensim.models.doc2vec import LabeledSentence
 from gensim.models import Doc2Vec
 import numpy as np
 
@@ -10,6 +11,18 @@ from sklearn.metrics import precision_recall_fscore_support
 from sklearn.cross_validation import KFold
 
 n_fold = 5
+
+
+class LabeledLineSentence(object):
+    def __init__(self, sources):
+        self.sources = sources
+
+    def __iter__(self):
+        raw_file = open(self.sources, 'r')
+        data = json.load(raw_file)
+        for entry_no, entry in enumerate(data):
+            label = entry['uid'] + '_' + entry['pid']
+            yield LabeledSentence(entry['rev'].encode('utf-8').split(), [label])
 
 
 def get_labels_ratings(full_file_name):
@@ -29,7 +42,8 @@ def read_labeled_doc(full_file_name):
     for entry in data:
         label = entry['uid'] + '_' + entry['pid']
         review = entry['rev']
-        yield doc2vec.TaggedDocument(words=review.split(), tags=[label.encode('utf-8')])
+        rat = entry['rat']
+        yield LabeledSentence(words=review.split(), tags=[label.encode('utf-8')]), rat
 
 
 def execute_model(X, y):
@@ -56,7 +70,7 @@ def prediction_model(X_train, y_train, X_test, y_test):
 
 if __name__ == '__main__':
     model = Doc2Vec(alpha=0.025, min_alpha=0.025, size=100, window=10, negative=5)
-    model.build_vocab(read_labeled_doc(path.join(util.data_path, util.file_name)))
+    model.build_vocab(read_labeled_doc(path.join(util.data_path, util.file_name))[0])
     for i in xrange(0, 1):
         model.train(read_labeled_doc(path.join(util.data_path, util.file_name)))
         print 'end of iteration ', i + 1
@@ -65,8 +79,8 @@ if __name__ == '__main__':
     y = []
 
     for entry in labels_ratings:
-        current_data = model[entry[0]]
-        current_rating = labels_ratings[entry][1]
+        current_data = model.docvecs[entry[0]]
+        current_rating = labels_ratings[entry[1]]
         X.append(current_data)
         y.append(y)
     X = np.asarray(X)
